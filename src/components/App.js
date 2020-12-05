fa82ee55fb2d603a9c976d2b660b576ff4252cde
Ayushi Sharma <sharmaroliayushi@gmail.com>
2020-12-05 17:22:12 +0530

    new

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,10 @@
-function App() {
+import React from "react";
+
+export default function App() {
   return(
+    <div>
 <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+</div>
   );
 }
 
-export default App;
